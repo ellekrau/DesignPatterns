f39f1436aa334f7ae31c40c706fd5657caef51f9
Adrielle Krauchuki <adrielle.krauchuki@ambevtech.com.br>
2021-07-06 22:52:13 -0300

    Implementação do factory method

---
+++ FactoryMethod/Entities/Personagens/Scorpion.cs
@@ -0,0 +1,10 @@
+﻿namespace FactoryMethod.Entities.Personagens
+{
+    public class Scorpion : IPersonagem
+    {
+        public void Escolhido()
+        {
+            System.Console.WriteLine("Você escolheu Scorpion");
+        }
+    }
+}
