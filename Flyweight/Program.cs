9abbefa6a13330cc228e80d9e88dc366ad2999da
Adrielle Krauchuki <adrielle.krauchuki@ambevtech.com.br>
2021-07-13 20:04:49 -0300

    Melhoria na implementação do Flyweight

--- Flyweight/Program.cs
+++ Flyweight/Program.cs
@@ -5,6 +5,7 @@ namespace Flyweight
 {
     static class Program
     {
+        const string barra = "\n ----------------------------------------------- \n";
         static void Main()
         {
             var flyweight = new Flyweight();
@@ -12,17 +13,20 @@ namespace Flyweight
 
             Tartaruga tartaruga;
 
+            Console.WriteLine(barra);
+
             while (true)
             {
                 Console.WriteLine(" Qual tartaruga enviar para a tela?");
                 Console.Write(" > ");
 
                 cor = Console.ReadLine();
+                Console.WriteLine();
 
                 tartaruga = flyweight.GetTartaruga(cor);
                 tartaruga.Mostra(cor);
 
-                Console.WriteLine("\n ------------------------ ");
+                Console.WriteLine(barra);
             }
         }
     }
